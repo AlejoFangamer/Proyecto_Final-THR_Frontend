9579acab8ac38b0b7608c0c9bc9eb753e19f9f0c
AlejoFangamer <xxalejoxx71@gmail.com>
2023-10-20 16:14:10 -0500

    valio

--- src/pages/Blog/script.js
+++ src/pages/Blog/script.js
@@ -5,6 +5,7 @@ import { menuAnimations } from "../Menu/script.js";
 import { startAnim } from "../startPageAnim.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  startAnim();
   getAllBlog();
   checkInputs();
   initFormulario();
