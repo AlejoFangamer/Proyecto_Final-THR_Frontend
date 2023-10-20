92f788dc3d95aa0f7be06337cdf99d9a4092c482
AlejoFangamer <xxalejoxx71@gmail.com>
2023-10-19 22:39:22 -0500

    Hola

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
@@ -7,6 +8,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         blog: resolve(__dirname, "src/pages/Blog/index.html"),
+        juegos: resolve(__dirname, "src/pages/Juegos/index.html"),
+        miembros: resolve(__dirname, "src/pages/Miembros/index.html"),
+        quienes: resolve(__dirname, "src/pages/Quienes/index.html"),
       },
     },
   },
